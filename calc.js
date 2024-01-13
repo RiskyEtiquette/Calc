168adb108b6f786ae904f55d76354793f9d06fa0
Kaden Davis <kadentreyd@gmail.com>
2024-01-13 12:57:22 -0700

    This is a git test

--- calc.js
+++ calc.js
@@ -14,6 +14,35 @@ reader.question("What would you like to calculate?", function(input){
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
 
+	console.log('mathSymbol', mathSymbol);
+	console.log('num1', num1);
+	console.log('num2', num2);
+
+	// This line adds addition
+	if (mathSymbol === "+"){
+			console.log(num1 + num2);
+	}
+
+	// This line add subtraction
+	if (mathSymbol === "-"){
+			console.log(num1 - num2);
+	}
+
+	// This line add multiplication
+	if (mathSymbol === "*"){
+			console.log(num1 * num2);
+	}
+
+	// This line adds division
+	if (mathSymbol === "/"){
+			console.log(num1 / num2);
+	}
+
+	// This line adds the square root
+	if (mathSymbol === "sqrt"){
+			console.log(Math.sqrt(num1));
+	}
+
 
 	// This line closes the connection to the command line interface.
 	reader.close()
